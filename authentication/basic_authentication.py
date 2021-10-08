0d45deafb993770b50735325a78dd30a7af9be93
Kaio Delphino <dash.zet@gmail.com>
2021-10-08 12:47:17 -0300

    altered directory structure

---
+++ authentication/basic_authentication.py
@@ -0,0 +1,11 @@
+import os
+
+# def get_api_key_from_request_header(request):
+#     return request.header["x-api-key"]
+
+
+# def is_authenticated(request):
+#     api_key = get_api_key_from_request_header(request)
+
+def is_authenticated():
+    # return 1
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -4,4 +4,4 @@ from authentication import basic_autentication as Auth
 
 
 def test_check_api_key_on_request_header():
-    ...
+    assert Auth.is_authenticated == 1
